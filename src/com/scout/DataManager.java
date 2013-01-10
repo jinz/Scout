e775ebe11adae65ec6a1420c6c76b9003a428372
Jinzhen Gong <gong.jinz@gmail.com>
2013-01-10 14:08:50 -0500

    Made DataManager a singleton and did more layouting on main menu.

--- src/com/scout/DataManager.java
+++ src/com/scout/DataManager.java
@@ -2,15 +2,25 @@ package com.scout;
 
 import java.util.HashMap;
 import java.util.ArrayList;
-import android.annotation.SuppressLint;
 import com.scout.data.*;
 
 public class DataManager {
 	
-	private HashMap<Integer, ArrayList<Integer>> teamToMatches;
-	private HashMap<Integer, ArrayList<Integer>> matchToTeams;
-	private HashMap<Integer, ScoutSheet> scoutData;
+	private HashMap<Integer, ArrayList<Team>> matchToTeams;
+	private HashMap<Integer, ArrayList<Match>> teamToMatches;
+	private static DataManager manager = null;
 	
+	protected DataManager()
+	{
+		matchToTeams = new HashMap<Integer, ArrayList<Team>>();
+		teamToMatches = new HashMap<Integer, ArrayList<Match>>();
+	}
+	public static DataManager getInstance()
+	{
+		if (manager == null)
+			return new DataManager();
+		return manager;
+	}
 	public static int hashTeamMatch(int team, int match)
 	{
 		return team * 1000000 + match;
--- src/com/scout/activities/MainActivity.java
+++ src/com/scout/activities/MainActivity.java
@@ -3,17 +3,19 @@ package com.scout.activities;
 import com.scout.R;
 import com.scout.R.layout;
 import com.scout.R.menu;
+import com.scout.*;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+	private DataManager manager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        manager = DataManager.getInstance();
     }
 
     @Override
